9f126dc7994ebfebff0e2bcdc6b3464d3b4370b9
Ravi Pandit <ravipandit1994@gmail.com>
2021-12-23 19:21:29 +0530

    1. BitmexStreamingTradeService class implemented getOrderChanges for get live order update from exchange 2. Bitmex Order class set clOrderId to userRefrence attribute

--- xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTradeService.java
+++ xchange-stream-bitmex/src/main/java/info/bitrich/xchangestream/bitmex/BitmexStreamingTradeService.java
@@ -4,9 +4,13 @@ import info.bitrich.xchangestream.bitmex.dto.BitmexOrder;
 import info.bitrich.xchangestream.core.StreamingTradeService;
 import io.reactivex.Observable;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.dto.Order;
+import org.knowm.xchange.dto.trade.UserTrade;
+import org.knowm.xchange.exceptions.NotYetImplementedForExchangeException;
+import org.knowm.xchange.instrument.Instrument;
 
 /** Created by Declan */
 public class BitmexStreamingTradeService implements StreamingTradeService {
@@ -32,4 +36,26 @@ public class BitmexStreamingTradeService implements StreamingTradeService {
                   .collect(Collectors.toList());
             });
   }
+
+    public Observable<Order> getOrderChanges() {
+        String channelName = "order";
+        return streamingService
+                .subscribeBitmexChannel(channelName)
+                .flatMapIterable(
+                        s -> {
+                            BitmexOrder[] bitmexOrders = s.toBitmexOrders();
+
+                            return Arrays.stream(bitmexOrders)
+                                    .map(BitmexOrder::toOrder)
+                                    .collect(Collectors.toList());
+                        });
+    }
+
+    public Observable<UserTrade> getUserTrades(Instrument instrument, Object... args) {
+        if (instrument instanceof CurrencyPair) {
+            return this.getUserTrades((CurrencyPair)instrument, args);
+        } else {
+            throw new NotYetImplementedForExchangeException("getUserTrades");
+        }
+    }
 }
